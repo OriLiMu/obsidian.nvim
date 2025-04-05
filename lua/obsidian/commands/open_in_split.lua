d4fd2596582f2841dc051f718b90e7a091afc89a
lovelife_lizhe <imlegendlzz@gmail.com>
2025-04-05 22:24:20 +0800

    feat: add q keymap to close split window

--- lua/obsidian/commands/open_in_split.lua
+++ lua/obsidian/commands/open_in_split.lua
@@ -2,13 +2,7 @@ local util = require "obsidian.util"
 local log = require "obsidian.log"
 
 -- Open note in right split window
-local function open_in_split()
-  local client = require("obsidian").get_client()
-  if not client then
-    log.err "Failed to get Obsidian client"
-    return
-  end
-
+local function open_in_split(client)
   -- Check if cursor is on a markdown link
   if util.cursor_on_markdown_link(nil, nil, true) then
     local link_location, link_name, link_type = util.parse_cursor_link()
@@ -18,9 +12,20 @@ local function open_in_split()
     local notes = { client:resolve_note(link_location) }
     if #notes > 0 then
       -- Open note in right split
+      local current_win = vim.api.nvim_get_current_win()
       client:open_note(notes[1], {
         open_strategy = "vsplit",
       })
+      -- Get the newly created window
+      local new_win = vim.api.nvim_get_current_win()
+      -- Only set the keymap if we're in a different window
+      if new_win ~= current_win then
+        -- Set local keymap for this buffer
+        vim.keymap.set("n", "q", function()
+          -- Close the window
+          vim.api.nvim_win_close(new_win, false)
+        end, { buffer = true, desc = "Close split window" })
+      end
     else
       vim.notify("Note not found: " .. (link_location or "empty link"), vim.log.levels.WARN)
     end
