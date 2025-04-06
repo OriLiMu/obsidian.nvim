97b7d2d69e8d98b2ebce920339f9fc09b7a6493e
lovelife_lizhe <imlegendlzz@gmail.com>
2025-04-06 14:08:14 +0800

    feat: open web URLs in default browser

--- lua/obsidian/commands/open_in_split.lua
+++ lua/obsidian/commands/open_in_split.lua
@@ -4,7 +4,15 @@ local log = require "obsidian.log"
 -- Open note in right split window
 local function open_in_split(client)
   if util.cursor_on_markdown_link(nil, nil, true) then
-    local link_location = util.parse_cursor_link()
+    local link_location, link_name, link_type = util.parse_cursor_link()
+
+    -- Check if it's a web URL
+    if link_type == "URL" or string.match(link_location, "^https?://") then
+      -- Use xdg-open on Linux to open URL in default browser
+      vim.fn.jobstart { "xdg-open", link_location }
+      return
+    end
+
     local notes = { client:resolve_note(link_location) }
 
     if #notes > 0 then
